b31ff320db0ff8c91feccbb62702789b3dab6ace
zatkh <zhtarkhani@gmail.com>
2021-08-22 18:42:19 +0100

    cleanup init

--- run.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/sh
-
-make QEMU_VIRTFS_ENABLE=y QEMU_USERNET_ENABLE=y QEMU_VIRTFS_HOST_DIR=/tmp/tee_share HOSTFWD=",hostfwd=tcp::1443-:1443" run-only
-#mount -t 9p -o trans=virtio host /mnt
-#darknetp classifier train -pp 4 /mnt/cfg/mnist.dataset /mnt/cfg/mnist_lenet.cfg
-#darknetp classifier train -pp 4 /mnt/cfg/mnist.dataset /mnt/cfg/mnist_lenet.cfg /mnt/models/mnist/mnist_lenet.weights
-
